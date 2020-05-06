fcd88edb900a7018b36389148fca1c610c5a2a22
usermine12 <mikaeljarv@gmail.com>
2020-05-06 16:32:33 +0300

    added themeprovider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ThemeProvider } from '@material-ui/core/styles'
+import './index.css'
+import App from './App'
+import { theme } from './theme'
 
 ReactDOM.render(
-  <App />,
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
   document.getElementById('root')
 )
---
+++ frontend/src/theme.js
@@ -0,0 +1,24 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#343434'
+    },
+    secondary: {
+      main: '#2C389A'
+    },
+    error: {
+      main: '#d32f2f'
+    },
+    warning: {
+      main: '#ffb74d'
+    },
+    info: {
+      main: '#64b5f6'
+    },
+  },
+  typography: {
+    
+  }
+})
