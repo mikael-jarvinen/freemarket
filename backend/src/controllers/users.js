fdc10c500507211b24ce9c38dedc56e966d77bff
usermine12 <mikaeljarv@gmail.com>
2020-05-08 00:17:20 +0300

    created baserouter for users

--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
-usersRouter.post('/', async (request, response) => {
+usersRouter.post('/', async (request, response, next) => {
   const body = request.body
 
   if (!body.displayName || !body.password) {
@@ -14,4 +14,44 @@ usersRouter.post('/', async (request, response) => {
       error: 'too short password, minimum length is 8'
     })
   }
+
+  const passwordHash = await bcrypt.hash(body.password, 10)
+
+  try {
+    let user = {
+      displayName: body.displayName,
+      passwordHash
+    }
+
+    if (body.firstName && body.lastName) {
+      user = {
+        ...user,
+        firstName: body.firstName,
+        lastName: body.lastName,
+      }
+    }
+
+    const newUser = new User(user)
+
+    response.json(await newUser.save())
+  } catch (e) {
+    next(e)
+  }
+})
+
+usersRouter.get('/:id', async (request, response, next) => {
+  const id = request.params.id
+
+  try {
+    const user = await User.findById(id)
+    response.json(user.toJSON())
+  } catch (e) {
+    next(e)
+  }
+})
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+
+  response.json(users.map(u => u.toJSON()))
 })
