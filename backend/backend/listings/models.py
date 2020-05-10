2be2bcb0288f447f92d4c1d63cc6d28a95685110
usermine12 <mikaeljarv@gmail.com>
2020-05-10 20:19:55 +0300

    made listing, user, review models

--- backend/backend/listings/models.py
+++ backend/backend/listings/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Listing(models.Model):
+  price = models.IntegerField()
+  title = models.CharField(max_length=25)
+  description = models.TextField(max_length=1000)
+  created = models.DateTimeField(auto_now_add=True)
+  postal_code = models.IntegerField(max_length=10)
+  author = models.ForeignKey('User', on_delete=models.CASCADE)
+
+class User(models.Model):
+  joined = models.DateTimeField(auto_now_add=True)
+  display_name = models.CharField(max_length=25)
+  full_name = models.CharField()
+  listings = models.ManyToManyField(Listing, null=True, on_delete=models.SET_NULL)
+  feedback = models.ManyToManyField('Review', null=True, on_delete=models.SET_NULL)
+
+class Review(models.Model):
+  FEEDBACK_CHOICES = [
+  ('POSITIVE', 'positive'),
+  ('NEUTRAL', 'neutral'),
+  ('NEGATIVE', 'negative')
+  ]
+  feedback = models.CharField(choices=FEEDBACK_CHOICES, default='NEGATIVE')
+  review = models.TextField(max_length=200)
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
+  created = models.DateTimeField(auto_now_add=True)
