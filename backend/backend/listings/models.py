456f711a0c292a6af5cc9cda5a7c1d672299ecd1
usermine12 <mikaeljarv@gmail.com>
2020-05-10 20:38:54 +0300

    added Question model for listings

--- backend/backend/listings/models.py
+++ backend/backend/listings/models.py
@@ -7,6 +7,7 @@ class Listing(models.Model):
   created = models.DateTimeField(auto_now_add=True)
   postal_code = models.IntegerField(max_length=10)
   author = models.ForeignKey('User', on_delete=models.CASCADE)
+  questions = models.ManyToManyField('Question', null=True, on_delete=models.SET_NULL)
 
 class User(models.Model):
   joined = models.DateTimeField(auto_now_add=True)
@@ -25,3 +26,9 @@ class Review(models.Model):
   review = models.TextField(max_length=200)
   author = models.ForeignKey(User, on_delete=models.CASCADE)
   created = models.DateTimeField(auto_now_add=True)
+
+class Question(models.Model):
+  created = models.DateTimeField(auto_now_add=True)
+  question = models.CharField(max_length=200)
+  reply = models.TextField(max_length=1000, null=True)
+  author = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
