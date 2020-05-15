445488c3c6d04c5170c220850bac55317d5a0f17
usermine12 <mikaeljarv@gmail.com>
2020-05-15 15:56:32 +0300

    added endpoint for root and removed order model

--- backend/backend/listings/views.py
+++ backend/backend/listings/views.py
@@ -8,6 +8,18 @@ from listings.serializers import (
     QuestionSerializer
 )
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'listings': reverse('snippet-list', request=request, format=format)
+    })
+
 
 class ListingList(generics.ListCreateAPIView):
     queryset = Listing.objects.all()
