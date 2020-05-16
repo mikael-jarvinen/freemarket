d15b22a53a0efe6839482be03edae223a8f20d2f
usermine12 <mikaeljarv@gmail.com>
2020-05-16 13:06:50 +0300

    removed explicit api root endpoint

--- backend/backend/listings/views.py
+++ backend/backend/listings/views.py
@@ -8,18 +8,6 @@ from listings.serializers import (
     QuestionSerializer
 )
 
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.reverse import reverse
-
-
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'listings': reverse('listing-list', request=request, format=format)
-    })
-
 
 class ListingViewSet(viewsets.ModelViewSet):
     queryset = Listing.objects.all()
