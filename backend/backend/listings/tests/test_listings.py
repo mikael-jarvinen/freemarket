4647a1f31dfab1bfe1bb3fa85f0a3efc93e16ced
usermine12 <mikaeljarv@gmail.com>
2020-05-18 16:19:57 +0300

    separated tests into separate files, and moved them to a tests directory

---
+++ backend/backend/listings/tests/test_listings.py
@@ -0,0 +1,103 @@
+from rest_framework.test import APIClient, APITestCase
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import status
+from listings.models import User, Listing
+from listings.serializers import UserSerializer, ListingSerializer
+
+
+class ListingTest(APITestCase):
+    """
+    Test module for testing endpoint for Listing model
+    """
+    user = 0
+
+    access_token = 0
+    refresh_token = 0
+
+    listing1 = 0
+    listing2 = 0
+
+    def setUp(self):
+        self.user = User.objects.create(
+            email='tester@gmail.com',
+            display_name='tester'
+        )
+        self.user.set_password('verylongpassword')
+        self.user.save()
+
+        refresh = RefreshToken.for_user(self.user)
+        self.access_token = str(refresh.access_token)
+        self.refresh_token = str(refresh)
+
+        self.listing1 = Listing.objects.create(
+            price=15.5,
+            title='testing1',
+            description='very good listing for testing',
+            postal_code=42000,
+            owner=self.user
+        )
+
+        self.listing2 = Listing.objects.create(
+            price=66.9,
+            title='testing2',
+            description='testing',
+            postal_code=54000,
+            owner=self.user
+        )
+
+    def test_database_returns_listings(self):
+        """Test that the database returns listings"""
+        listing1 = Listing.objects.get(id=self.listing1.id)
+        listing2 = Listing.objects.get(id=self.listing2.id)
+
+        self.assertEqual(self.listing1, listing1)
+        self.assertEqual(self.listing2, listing2)
+
+    def test_api_returns_listings(self):
+        """Test that the api returns listings"""
+        client = APIClient()
+        response = client.get('/api/listings/')
+        listing1 = response.data['results'][0]
+        listing2 = response.data['results'][1]
+        self.assertEqual(
+            listing1,
+            ListingSerializer(self.listing1).data
+        )
+        self.assertEqual(
+            listing2,
+            ListingSerializer(self.listing2).data
+        )
+
+    def test_api_returns_listing(self):
+        """Test that the api returns a single listing"""
+        client = APIClient()
+        response = client.get(f'/api/listings/{self.listing1.id}/')
+        self.assertEqual(
+            response.data,
+            ListingSerializer(self.listing1).data
+        )
+
+    def test_api_listing_posting(self):
+        client = APIClient()
+        client.credentials(
+            HTTP_AUTHORIZATION='Bearer ' + self.access_token
+        )
+        response = client.post(
+            '/api/listings/',
+            {
+                'price': 4.5,
+                'title': 'listing',
+                'description': 'posted listing',
+                'postal_code': 26500
+            }
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(
+            response.data,
+            ListingSerializer(Listing.objects.get(id=3)).data
+        )
+        owner_id = response.data['owner']
+        self.assertEqual(
+            UserSerializer(User.objects.get(id=owner_id)).data,
+            UserSerializer(self.user).data
+        )
---
+++ backend/backend/listings/tests/test_users.py
@@ -0,0 +1,89 @@
+from rest_framework.test import APIClient, APITestCase
+from listings.models import User
+from listings.serializers import UserSerializer
+
+
+class UserTest(APITestCase):
+    """Test module for users"""
+
+    INITIAL_USERS = [0, 0]
+
+    def setUp(self):
+        self.INITIAL_USERS[0] = User.objects.create(
+            email='tester1@gmail.com',
+            display_name='tester1'
+        )
+        self.INITIAL_USERS[1] = User.objects.create(
+            email='tester2@gmail.com',
+            display_name='tester2',
+            full_name='tester two',
+            biography='User here for testing',
+            website='http://tester2.com'
+        )
+
+    def test_database_returns_users(self):
+        """Test that the database returns users"""
+        self.assertEqual(
+            User.objects.get(id=self.INITIAL_USERS[0].id),
+            self.INITIAL_USERS[0]
+        )
+        self.assertEqual(
+            User.objects.get(id=self.INITIAL_USERS[1].id),
+            self.INITIAL_USERS[1]
+        )
+
+    def test_api_returns_users(self):
+        """Test that the API returns users"""
+        client = APIClient()
+        response = client.get('/api/users/', format='json')
+        tester1 = response.data['results'][0]
+        tester2 = response.data['results'][1]
+        self.assertEqual(
+            tester1,
+            UserSerializer(self.INITIAL_USERS[0]).data
+        )
+        self.assertEqual(
+            tester2,
+            UserSerializer(self.INITIAL_USERS[1]).data
+        )
+
+    def test_api_returns_user(self):
+        """Test that the api returns single user"""
+        client = APIClient()
+        user_id = self.INITIAL_USERS[0].id
+        response = client.get(f'/api/users/{user_id}/', format='json')
+        self.assertEqual(
+            response.data,
+            UserSerializer(self.INITIAL_USERS[0]).data
+        )
+
+    def test_api_posting_users(self):
+        client = APIClient()
+        response = client.post(
+            '/api/users/',
+            {
+                'email': 'tester3@gmail.com',
+                'display_name': 'tester3',
+            },
+            format='json'
+        )
+        self.assertEqual(
+            response.data,
+            UserSerializer(User.objects.get(id=3)).data
+        )
+        response = client.post(
+            '/api/users/',
+            {
+                'email': 'tester4@gmail.com',
+                'display_name': 'tester4',
+                'full_name': 'tester 4',
+                'biography': 'tester number 4',
+                'website': 'http://testers.com',
+                'password': 'secret'
+            },
+            format='json'
+        )
+        self.assertEqual(
+            response.data,
+            UserSerializer(User.objects.get(id=4)).data
+        )
