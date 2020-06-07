d2a1d33e092ea6d589f117dc4d7a559ddba06734
usermine12 <mikaeljarv@gmail.com>
2020-06-07 14:09:26 +0300

    added home button to redirect to root in drawer

--- frontend/src/components/Drawer.js
+++ frontend/src/components/Drawer.js
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import { useHistory } from 'react-router-dom'
-import { Drawer, Box } from '@material-ui/core'
+import { Drawer, Box, Divider } from '@material-ui/core'
 import TextButton from './TextButton'
 
 const DrawerComponent = () => {
@@ -12,12 +12,35 @@ const DrawerComponent = () => {
     <Drawer
       variant='permanent'
     >
-      <Box marginTop={10} width='13vw'>
-        <Box margin={1}>
-          <TextButton
-            onClick={() => history.push('/listings?page=1')}
-            text='browse listings'
-          />
+      <Box
+        marginTop={10}
+        width='13vw'
+        display='flex'
+        flexDirection='column'
+        flexGrow={1}
+      >
+        <Box
+          margin={1}
+          flexGrow={1}
+          display='flex'
+          flexDirection='column'
+        >
+          <Box marginTop={2}>
+            <TextButton
+              onClick={() => history.push('/')}
+              text='home'
+            />
+          </Box>
+          <Box marginTop={2}>
+            <TextButton
+              onClick={() => history.push('/listings?page=1')}
+              text='browse listings'
+            />
+          </Box>
+          <Divider/>
+          <Box display='flex' flexGrow={1} flexDirection='column-reverse'>
+            hello
+          </Box>
         </Box>
       </Box>
     </Drawer>
