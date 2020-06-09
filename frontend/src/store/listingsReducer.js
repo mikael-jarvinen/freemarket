316268b990ecbcd6c4d165c13db253998f0eeb8d
usermine12 <mikaeljarv@gmail.com>
2020-06-09 17:45:50 +0300

    removed loadOwner action creator and corresponding action type from reducer

--- frontend/src/store/listingsReducer.js
+++ frontend/src/store/listingsReducer.js
@@ -3,7 +3,6 @@
 // and 2 recently viewed pages of listings
 
 import { get } from '../services/listingService'
-import { getById } from '../services/userService'
 import _ from 'lodash'
 
 const initialState = {
@@ -13,7 +12,7 @@ const initialState = {
   resolving: false
 }
 
-// Returns action creators that load a new page, set count of pages
+// Returns action creators that loads a new page, set count of pages
 // and if filters are unequal to current filters set new filters and
 // clear current cached filters
 export const loadPage = (page, filters) => {
@@ -64,34 +63,6 @@ export const loadPage = (page, filters) => {
   }
 }
 
-// populates the owner field by listing id
-export const loadOwner = listingId => {
-  return async (dispatch, getState) => {
-    const pages = getState().listings.pages
-    let pageKey = null
-    for (const key in pages) {
-      const page = pages[key]
-      if (page.listings.find(({ id }) => id === listingId)) {
-        pageKey = key
-        break
-      }
-    }
-
-    const listings = pages[pageKey].listings
-    const userId = listings.find(({ id }) => id === listingId).owner
-    const owner = await getById(userId)
-
-    dispatch({
-      type: 'POPULATE_OWNER',
-      data: {
-        page: pageKey,
-        listing: listingId,
-        owner
-      }
-    })
-  }
-}
-
 const listingsReducer = (state = initialState, action) => {
   switch (action.type) {
   case 'LOAD_PAGE':
@@ -100,26 +71,6 @@ const listingsReducer = (state = initialState, action) => {
       pages: {
         ...state.pages,
         [`${action.data.page}`]: action.data
-      }
-    }
-  case 'POPULATE_OWNER':
-    return {
-      ...state,
-      pages: {
-        ...state.pages,
-        [action.data.page]: {
-          ...action.data.page,
-          listings: state.pages[action.data.page].listings.map(l => {
-            if (l.id === action.data.listing) {
-              return {
-                ...l,
-                owner: action.data.owner
-              }
-            } else {
-              return l
-            }
-          })
-        }
       }
     }
   case 'SET_PAGE_COUNT':
